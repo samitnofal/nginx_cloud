7c6029769637e6b542e333f40339d8abf1a3bcbc
Sami Nofal <samitnofal@gmail.com>
2023-06-22 03:15:24 +0300

    Move Stop and delete to pipeline

--- run_test_curl_on_server.sh
+++ run_test_curl_on_server.sh
@@ -12,11 +12,8 @@ echo $service_url
 # Make a curl request to the service URL and echo the response
 curl_response=$(curl -s "$service_url")
 
+echo "$curl_response"
 if ! echo "$curl_response" | grep -q "Welcome to Cloud"; then
   echo "String not found: Welcome to Cloud"
   exit 1  # Fail the script
 fi
-
-echo "$curl_response"
-minikube stop
-minikube delete
